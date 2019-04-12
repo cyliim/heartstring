75a0cb3162fdea269f0c488409c0737bc3b2504c
Brickman <brickmanbots@gmail.com>
2019-04-13 11:31:47 +1200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const config = require("./config.json");
 //8ball list
 var ball =["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", " Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
 client.on("ready", () => {
-    client.user.setActivity(`h.help`)
+    client.user.setActivity(`h.help || ${client.guilds.size} servers`)
       });
   client.on("message", async message => {
     const date = message.author.createdAt;
