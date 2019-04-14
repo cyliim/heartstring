23f4e4c7fd0d1768c82bb38bf8a729308fa748ba
Brickman <brickmanbots@gmail.com>
2019-04-14 16:31:52 +1200

    added reddit command

--- index.js
+++ index.js
@@ -234,26 +234,49 @@ if (message.content.startsWith(config.prefix + "purge")) {
       .setThumbnail(message.guild.iconURL)
   message.channel.send({embed});
   
-} if (message.content.startsWith(config.prefix + "meme")) {
-  try {
-    const { body } = await snekfetch
-        .get('https://www.reddit.com/r/dankmemes.json?sort=top&t=week')
-        .query({ limit: 800 });
-    const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
-    if (!allowed.length) return message.channel.send('It seems we are out of fresh memes!, Try again later.');
-    const randomnumber = Math.floor(Math.random() * allowed.length)
-    const memeembed = new Discord.RichEmbed()
-    .setColor(0x333333)
-    .setTitle(allowed[randomnumber].data.title)
-    .setDescription("Posted by: u/" + allowed[randomnumber].data.author)
-    .setImage(allowed[randomnumber].data.url)
-    .addField("Other info:", "Upvotes: " + allowed[randomnumber].data.ups + " | Comments: " + allowed[randomnumber].data.num_comments)
-    .setFooter("Memes provided by r/dankmemes")
-    message.channel.send(memeembed)
-} catch (err) {
-    return console.log(err);
-}
+  } if (message.content.startsWith(config.prefix + "meme")) {
+    var subs = [`https://www.reddit.com/r/dankmemes.json?sort=top&t=week`, `https://www.reddit.com/r/memes.json?sort=top&t=week`, `https://www.reddit.com/r/dank_meme.json?sort=top&t=week`];
+    const randomsubs = subs[Math.floor(Math.random() * subs.length)];
+    try {
+      const { body } = await snekfetch
+          .get(randomsubs)
+          .query({ limit: 800 });
+      const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
+      if (!allowed.length) return message.channel.send('It seems we are out of fresh memes!, Try again later.');
+      const randomnumber = Math.floor(Math.random() * allowed.length)
+      const memeembed = new Discord.RichEmbed()
+      .setColor(0x333333)
+      .setTitle(allowed[randomnumber].data.title)
+      .setDescription("Posted by: u/" + allowed[randomnumber].data.author)
+      .setImage(allowed[randomnumber].data.url)
+      .addField("Other info:", "Upvotes: " + allowed[randomnumber].data.ups + " | Comments: " + allowed[randomnumber].data.num_comments)
+      .setFooter("Memes provided by reddit")
+      message.channel.send(memeembed)
+  } catch (err) {
+      return console.log(err);
+  }
+  } if (message.content.startsWith(config.prefix + "reddit")) {
+    if (!args) return message.channel.send("Please provide a subreddit (i.e dankmemes, softwaregore)");
+    var sub = "https://www.reddit.com/r/" + args + ".json?sort=top&t=week"
+    try {
+      const { body } = await snekfetch
+          .get(sub)
+          .query({ limit: 800 });
+      const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
+      if (!allowed.length) return message.channel.send('It seems we are out of fresh memes!, Try again later.');
+      const randomnumber = Math.floor(Math.random() * allowed.length)
+      const subembed = new Discord.RichEmbed()
+      .setColor(0x333333)
+      .setTitle(allowed[randomnumber].data.title)
+      .setDescription("Posted by: u/" + allowed[randomnumber].data.author)
+      .setImage(allowed[randomnumber].data.url)
+      .addField("Other info:", "Upvotes: " + allowed[randomnumber].data.ups + " | Comments: " + allowed[randomnumber].data.num_comments)
+      .setFooter("Provided by reddit")
+      message.channel.send(subembed)
+  } catch (err) {
+      return console.log(err);
 
+  }
 //botinfo
 
     } if (message.content.startsWith(config.prefix + "bi")) {
