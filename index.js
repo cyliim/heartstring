9f4f2a8e2817f62f236c2fe9d2c68fb7ba28d546
Brickman <brickmanbots@gmail.com>
2019-04-24 17:36:31 +1200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const snekfetch = require("snekfetch")
 //8ball list
 var ball =["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", " Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
 client.on("ready", () => {
-    client.user.setActivity(`hd.help || ${client.guilds.size} servers`)
+    client.user.setActivity(`h.help || ${client.guilds.size} servers`)
       });
   client.on("message", async message => {
     const date = message.author.createdAt;
